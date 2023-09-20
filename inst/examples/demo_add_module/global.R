83b52066b990b62fac6f7336780afdbe69d9fd0b
adamkucharski <adam.kucharski@lshtm.ac.uk>
2023-09-20 12:24:35 +0200

    Add vignette and new module

---
+++ inst/examples/demo_add_module/global.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(bslib)
+library(epishiny)
+
+# options(
+#   "epishiny.na.label" = "(Manquant)",
+#   "epishiny.week.letter" = "S"
+# )
+
+app_title = "epishiny modules"
+app_font <- "Roboto Mono"
+
+# example package data
+data("df_ll") # linelist
+data("sf_yem") # sf geo boundaries for Yemen admin 1 & 2
+
+# setup geo data for adm1 and adm2 in the format
+# required for epishiny map module
+geo_data <- list(
+  "adm1" = list(
+    level_name = "Governorate",
+    sf = sf_yem$adm1,
+    name_var = "adm1_name",
+    join_by = c("pcode" = "adm1_pcode")
+  ),
+  "adm2" = list(
+    level_name = "District",
+    sf = sf_yem$adm2,
+    name_var = "adm2_name",
+    join_by = c("pcode" = "adm2_pcode")
+  )
+)
+
+# range of dates used in filter module to filter time period
+date_range <- range(df_ll$date_notification, na.rm = TRUE)
+
+# define date variables in data as named list to be used in app
+date_vars <- c(
+  "Date of notification" = "date_notification",
+  "Date of onset" = "date_symptom_start",
+  "Date of hospitalisation" = "date_hospitalisation_start",
+  "Date of outcome" = "date_hospitalisation_end"
+)
+
+# define categorical grouping variables
+# in data as named list to be used in app
+group_vars <- c(
+  "Governorate" = "adm1_origin",
+  "Sex" = "sex_id",
+  "Hospitalised" = "hospitalised_yn",
+  "Vaccinated measles" = "vacci_measles_yn",
+  "Outcome" = "outcome"
+)
+
+epi_pal <- list(
+  PRIMARY = "#2E569E",
+  SECONDARY = "#D37331",
+  SUCCESS = "#94BA3B"
+)
+
---
+++ inst/examples/demo_add_module/server.R
@@ -0,0 +1,56 @@
+server <- function(input, output, session) {
+
+  app_data <- filter_server(
+    id = "filter",
+    df_ll = df_ll,
+    date_var = "date_notification",
+    group_vars = group_vars
+  )
+
+  place_server(
+    id = "map",
+    df_ll = reactive(app_data()$df_ll),
+    geo_data = geo_data,
+    group_vars = group_vars,
+    filter_info = reactive(app_data()$filter_info)
+  )
+
+  epicurve_click <- time_server(
+    id = "curve",
+    df_ll = reactive(app_data()$df_ll),
+    date_vars = date_vars,
+    group_vars = group_vars,
+    ratio_var = "outcome",
+    ratio_lab = "CFR",
+    ratio_numer = "Deceased",
+    ratio_denom = c("Deceased", "Healed", "Abandonment"),
+    filter_info = reactive(app_data()$filter_info)
+  )
+  
+  transmission_click <- transmission_server(
+    id = "transmission",
+    df_ll = reactive(app_data()$df_ll),
+    date_vars = date_vars,
+    group_vars = group_vars,
+    ratio_var = "outcome",
+    ratio_lab = "CFR",
+    ratio_numer = "Deceased",
+    ratio_denom = c("Deceased", "Healed", "Abandonment"),
+    filter_info = reactive(app_data()$filter_info)
+  )
+
+  # observe({
+  #   print(epicurve_click())
+  # })
+
+  person_server(
+    id = "age_sex",
+    df_ll = reactive(app_data()$df_ll),
+    age_var = "age_years",
+    sex_var = "sex_id",
+    male_level = "Male",
+    female_level = "Female",
+    filter_info = reactive(app_data()$filter_info)
+  )
+
+}
