d7ae33d49320a426e66c165a37b87bf23bdc3937
Paul Campbell <pacampbell91@gmail.com>
2023-11-17 12:07:12 +0100

    some code improvements to person module

--- R/03_person.R
+++ R/03_person.R
@@ -58,15 +58,15 @@ person_ui <- function(
     bslib::nav_panel(
       title = shiny::icon("chart-bar"),
       highcharter::highchartOutput(ns("as_pyramid"))
+    ),
+    bslib::nav_panel(
+      title = bsicons::bs_icon("table"),
+      tags$div(
+        id = ns("as_tbl_container"),
+        style = "min-height: 300px;",
+        gt::gt_output(ns("as_tbl"))
+      )
     )
-    # bslib::nav_panel(
-    #   title = shiny::icon("table"),
-    #   tags$div(
-    #     id = ns("as_tbl_container"),
-    #     style = "min-height: 300px;",
-    #     gt::gt_output(ns("as_tbl"))
-    #   )
-    # )
   )
 
 }
@@ -126,12 +126,37 @@ person_server <- function(
       )
 
       df_mod <- reactive({
-        df <- force_reactive(df)
-        if (!is.factor(df[[sex_var]])) {
-          df[[sex_var]] <- forcats::fct_na_value_to_level(
-            df[[sex_var]],
+        df <- force_reactive(df) 
+        df$sex <- df[[sex_var]]
+        # ensure sex var is factor
+        if (!is.factor(df$sex)) {
+          df$sex <- forcats::fct_na_value_to_level(
+            df$sex,
             getOption("epishiny.na.label", "(Missing)")
           )
+        }
+        # bin ages if we're working with a numeric variable
+        if (is.null(age_group_var)) {
+          if (is.null(age_var)) {
+            cli::cli_abort("if {.arg age_group_var} if not provided then {.arg age_var} must be.", call = NULL)
+          }
+          if (!is.numeric(df[[age_var]])) {
+            cli::cli_abort("{.arg age_group} must be numeric. Use {.arg age_group} instead if ages have already been binned into groups.", call = NULL)
+          }
+          df <- df %>% bin_ages(age_var, age_breaks, age_labels)
+        } else {
+          # ensure age_group is a factor
+          df$age_group <- df[[age_group_var]]
+          if (!is.factor(df$age_group)) {
+            cli::cli_warn(
+              "{.arg age_group_var} should be a factor. Coercing to factor but levels may not be in the desired order.",
+              call = "person_server()"
+            )
+            df$age_group <- forcats::fct_na_value_to_level(
+              df$age_group,
+              getOption("epishiny.na.label", "(Missing)")
+            )
+          }
         }
         df
       })
@@ -157,7 +182,6 @@ person_server <- function(
           df_age_sex = hc_dat$df_age_sex,
           missing_age = hc_dat$missing_age,
           missing_sex = hc_dat$missing_sex,
-          sex_var = sex_var,
           colours = colours,
           ylab = age_group_lab,
           value_name = get_label(input$count_var, count_vars),
@@ -171,21 +195,28 @@ person_server <- function(
         w_tbl$show()
         on.exit(w_tbl$hide())
 
-        df_mod() %>%
-          dplyr::select(dplyr::all_of(c(sex_var, age_var))) %>%
-          bin_ages(age_var, age_breaks, age_labels) %>%
+        df_gt <- df_mod()
+
+        if (length(count_vars)) {
+          df_gt <- df_gt %>% 
+            dplyr::select(.data$sex, .data$age_group, .data[[input$count_var]]) %>% 
+            tidyr::uncount(.data[[input$count_var]])
+        } 
+
+        df_gt %>%
+          dplyr::select(.data$sex, .data$age_group) %>% 
           gtsummary::tbl_summary(
-            by = sex_var,
+            by = "sex",
             label  = list(
-              age_var ~ age_var_lab,
+              # age_var ~ age_var_lab,
               "age_group" ~ age_group_lab
             ),
-            type = list(age_var ~ "continuous2"),
-            digits = list(age_var ~ c(2, 0, 0, 0, 0, 0)),
             missing_text = getOption("epishiny.na.label", "(Missing)"),
-            statistic = gtsummary::all_continuous() ~ c("{mean}",
-                                                        "{median} ({p25}, {p75})",
-                                                        "{min}, {max}")
+            # type = list(age_var ~ "continuous2"),
+            # digits = list(age_var ~ c(2, 0, 0, 0, 0, 0)),
+            # statistic = gtsummary::all_continuous() ~ c("{mean}",
+            #                                             "{median} ({p25}, {p75})",
+            #                                             "{min}, {max}")
           ) %>%
           gtsummary::modify_header(
             gtsummary::all_stat_cols() ~ "**{level}**, N = {n} ({gtsummary::style_percent(p, digits = 1)}%)"
@@ -194,28 +225,18 @@ person_server <- function(
           gtsummary::italicize_levels() %>%
           gtsummary::modify_footnote(update = gtsummary::everything() ~ NA) %>%
           gtsummary::bold_labels() %>%
-          gtsummary::as_gt() # %>%
-          # gt::opt_interactive(
-          #   use_compact_mode = TRUE,
-          #   use_pagination = FALSE,
-          #   use_pagination_info = FALSE,
-          #   use_page_size_select = FALSE,
-          #   use_sorting = FALSE,
-          #   use_highlight = TRUE
-          # )
+          gtsummary::as_gt() 
       })
 
     }
   )
 }
 
-
 #' @noRd
 hc_as_pyramid <- function(
     df_age_sex,
     missing_age,
     missing_sex,
-    sex_var,
     value_name = "Patients",
     value_digit = 0,
     value_unit = "",
@@ -232,11 +253,11 @@ hc_as_pyramid <- function(
   xaxis <- list(categories = x_levels, reversed = FALSE, title = list(text = ylab))
 
   series <- df_age_sex %>%
-    dplyr::group_by(.data[[sex_var]]) %>%
+    dplyr::group_by(.data$sex) %>%
     dplyr::arrange(.data$age_group) %>% 
     dplyr::do(data = .data$n) %>%
     dplyr::ungroup() %>%
-    dplyr::rename(name = .data[[sex_var]]) %>%
+    dplyr::rename(name = .data$sex) %>%
     highcharter::list_parse()
 
   hc_out <- highcharter::highchart() %>%
@@ -290,6 +311,7 @@ hc_as_pyramid <- function(
   hc_out %>% my_hc_export(caption = filter_info, width = 700)
 }
 
+#' @noRd 
 get_as_df <- function(
   df,
   sex_var,
@@ -301,56 +323,38 @@ get_as_df <- function(
   age_labels = c("<5", "5-17", "18-24", "25-34", "35-49", "50+"),
   count_var = NULL
 ) {
-  # browser()
-  # bin ages if we're working with a numeric variable
-  if (is.null(age_group_var)) {
-    if (is.null(age_var)) {
-      cli::cli_abort("if {.arg age_group_var} if not provided then {.arg age_var} must be.", call = NULL)
-    }
-    df <- df %>% bin_ages(age_var, age_breaks, age_labels)
-  } else {
-    # ensure age_group is a factor
-    df <- df %>% dplyr::mutate(age_group := .data[[age_group_var]])
-    if (!is.factor(df$age_group)) {
-      cli::cli_warn(
-        "{.arg age_group_var} should be a factor. Coercing to factor but levels may not be in the desired order.",
-        call = "person_server()"
-      )
-      df$age_group <- factor(df$age_group)
-    }
-  }
 
   # get missing data numbers depending on whether data is pre-aggregated or not
   if (length(count_var)) {
     missing_sex <- df %>% 
-      dplyr::filter(!.data[[sex_var]] %in% c(male_level, female_level) | is.na(.data[[sex_var]])) %>% 
+      dplyr::filter(!.data$sex %in% c(male_level, female_level) | is.na(.data$sex)) %>% 
       dplyr::pull(.data[[count_var]]) %>% 
       sum()
     missing_age <- df %>% 
-      dplyr::filter(is.na(.data$age_group) | age_group == getOption("epishiny.na.label", "(Missing)")) %>% 
+      dplyr::filter(is.na(.data$age_group) | .data$age_group == getOption("epishiny.na.label", "(Missing)")) %>% 
       dplyr::pull(.data[[count_var]]) %>%
       sum()
   } else {
-    missing_sex <- nrow(dplyr::filter(df, !.data[[sex_var]] %in% c(male_level, female_level) | is.na(.data[[sex_var]])))
+    missing_sex <- nrow(dplyr::filter(df, !.data$sex %in% c(male_level, female_level) | is.na(.data$sex)))
     missing_age <- sum(is.na(df$age_group) | df$age_group == getOption("epishiny.na.label", "(Missing)"))
   }
   
   df_age_sex <- df %>%
-    dplyr::filter(.data[[sex_var]] %in% c(male_level, female_level)) %>%
-    dplyr::mutate(!!rlang::sym(sex_var) := droplevels(.data[[sex_var]]))
+    dplyr::filter(.data$sex %in% c(male_level, female_level)) %>%
+    dplyr::mutate(!!rlang::sym("sex") := droplevels(.data$sex))
   # if data is pre-aggregated add the count_var weight to the count function
   if (length(count_var)) {
     df_age_sex <- df_age_sex %>%
-      dplyr::count(.data[[sex_var]], .data$age_group, wt = .data[[count_var]])
+      dplyr::count(.data$sex, .data$age_group, wt = .data[[count_var]])
   } else {
     df_age_sex <- df_age_sex %>%
-      dplyr::count(.data[[sex_var]], .data$age_group)
+      dplyr::count(.data$sex, .data$age_group)
   }
   df_age_sex <- df_age_sex %>%
-    dplyr::mutate(n = dplyr::if_else(.data[[sex_var]] == male_level, -.data$n, .data$n)) %>%
-    tidyr::complete(.data[[sex_var]], .data$age_group, fill = list(n = 0)) %>%
-    dplyr::filter(!is.na(.data[[sex_var]]), !is.na(.data$age_group)) %>%
-    dplyr::arrange(.data[[sex_var]], .data$age_group)
+    dplyr::mutate(n = dplyr::if_else(.data$sex == male_level, -.data$n, .data$n)) %>%
+    tidyr::complete(.data$sex, .data$age_group, fill = list(n = 0)) %>%
+    dplyr::filter(!is.na(.data$sex), !is.na(.data$age_group)) %>%
+    dplyr::arrange(.data$sex, .data$age_group)
   
   tibble::lst(df_age_sex, missing_age, missing_sex)
 }
